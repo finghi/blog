c4f091b460ecce8dabf5d4560dcb667637265d53
finghi <728705199@qq.com>
2024-10-06 19:00:50 +0800

    加入base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,28 +1,25 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig } from 'vuepress'
 
-import theme from "./theme.js";
+import theme from './theme.js'
 
 export default defineUserConfig({
-  base: "/",
+  base: '/blog/',
 
-  lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
-  
+  lang: 'zh-CN',
+  title: '博客演示',
+  description: 'vuepress-theme-hope 的博客演示',
+
   theme,
-  
-  extendsPageOptions:(extendable, app)=>{
-   
 
+  extendsPageOptions: (extendable, app) => {
     if (extendable.path === '/404.html') {
-       console.log(app);
-    console.log(extendable);
-    
-      extendable.frontmatter.layout = 'NotFound';
+      console.log(app)
+      console.log(extendable)
+
+      extendable.frontmatter.layout = 'NotFound'
     }
-    
   },
 
   // 和 PWA 一起启用
-  shouldPrefetch: false,
-});
+  shouldPrefetch: false
+})
